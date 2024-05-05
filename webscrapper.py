f2e4a26e8ea3d0689b31cc4b5b833f483442406e
Italo <italo.casanova.d@uni.pe>
2024-05-04 19:53:41 -0500

    add:output

--- webscrapper.py
+++ webscrapper.py
@@ -1,18 +1,27 @@
 import asyncio
 from twscrape import API, gather
 from twscrape.logger import set_log_level
+import json
 
 async def main():
     api = API()
 
-    q = "interbank since:2023-01-01 until:2023-05-31"
-    tweets = []
-    async for rep in api.search_raw(q, limit=5000):
-        # rep is httpx.Response object
-        if rep.status_code == 200:
-            tweets.extend(rep.json())
-        else:
-            print(rep.status_code, rep.json())
+    # create a json file to store the tweets
+    with open('tweets.json', 'w') as f:
+        q = "backus since:2023-01-01 until:2023-05-31"
+        tweets = []
+        async for rep in api.search_raw(q, limit=5000):
+            # rep is httpx.Response object
+            if rep.status_code == 200:
+                tweets.extend(rep.json())
+                print(rep.status_code, rep.json())
+            else:
+                print(rep.status_code, rep.json())
+
+        json.dump(tweets, f, indent=4)
+    # save the tweets list into a file
+
+
 
 
 
